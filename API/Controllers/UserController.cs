1a47cfcc064b45794bb728861f07a8aeccbe4134
z-aghaei <z-aghaei@88065-T4SH>
2025-03-02 11:16:41 +0330

    Add project files.

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpPost]
+        public int Create(Domain.User user)
+        {
+            return 1;
+        }
+    }
+}
