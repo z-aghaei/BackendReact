032540de0c1887496e622878972c81512e2ed1d5
neayani <neayani@gmail.com>
2025-03-02 12:02:25 +0330

    .

---
+++ Application/User/CreateUserCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.User
+{
+    public class CreateUserCommandHandler : IRequestHandler<CreateUserCommand, int>
+    {
+        public Task<int> Handle(CreateUserCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
